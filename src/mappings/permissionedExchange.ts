898b8d3117faa92c8ab0c8d0f9edad20ce1c83fc
Lachlan McCrae <8554309+lamcc21@users.noreply.github.com>
2022-12-08 10:24:00 +1300

    Moonbase (#14)
    * SQN-779: Migrate from acala evm to frontier evm (#6)
    
    * add chaintypes, update deps, update project.yaml
    
    * update types used from mapping functions
    
    * Update package.json
    
    Co-authored-by: HAO ZHAI <zhaihaohk@gmail.com>
    
    * Update project.yml (#7)
    
    * refactor chaintypes file
    
    * Update license and dependency
    
    * fix: exhange subql project
    
    * fix: hander trade
    
    * remove comment
    
    * Upgrade contracts
    
    * update: start block
    
    * update project for new claimrewards event (#10)
    
    * update project for new claimrewards event
    
    * remove rewardsDistributor file
    
    * Update with contract changes
    
    * update contract addresses, start block, contract-sdk (#12)
    
    Co-authored-by: Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
    
    * update contract address, contract sdk, start block (#13)
    
    Co-authored-by: Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
    
    Co-authored-by: Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
    Co-authored-by: HAO ZHAI <zhaihaohk@gmail.com>
    Co-authored-by: CherryLiang <cherryliang0112@gmail.com>

--- src/mappings/permissionedExchange.ts
+++ src/mappings/permissionedExchange.ts
@@ -1,5 +1,8 @@
-import { AcalaEvmEvent } from "@subql/acala-evm-processor";
-import { ExchangeOrderSentEvent, OrderSettledEvent, TradeEvent } from "@subql/contract-sdk/typechain/PermissionedExchange";
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { FrontierEvmEvent } from "@subql/frontier-evm-processor";
+import { ExchangeOrderSentEvent, OrderSettledEvent, TradeEvent, QuotaAddedEvent, PermissionedExchange } from "@subql/contract-sdk/typechain/PermissionedExchange";
 import assert from "assert";
 import { OrderStatus } from "../types";
 
@@ -13,13 +16,13 @@ const { ACTIVE, INACTIVE } = OrderStatus;
 
 function calculateTradeAmount(
     totalTradeAmount: bigint,
-    event: AcalaEvmEvent<TradeEvent['args']>
+    event: FrontierEvmEvent<TradeEvent['args']>
 ): bigint {
     const {tokenGet, amountGet} = event.args;
     const getIsKSQT = isKSQT(tokenGet); 
     const tradeAmountBN = BigNumber.from(totalTradeAmount);
 
-    if(getIsKSQT) return tradeAmountBN.add(amountGet).toBigInt();
+    if (getIsKSQT) return tradeAmountBN.add(amountGet).toBigInt();
 
     return totalTradeAmount;
 }
@@ -27,7 +30,7 @@ function calculateTradeAmount(
 async function createTrade(
     orderId: BigNumber,
     sender: string,
-    event: AcalaEvmEvent<TradeEvent['args']>
+    event: FrontierEvmEvent<TradeEvent['args']>
 ): Promise<void> {
     const { tokenGive, tokenGet, amountGive, amountGet } = event.args;
 
@@ -44,60 +47,11 @@ async function createTrade(
     await trade.save();   
 }
 
-export async function handleExchangeOrderSent(
-    event: AcalaEvmEvent<ExchangeOrderSentEvent['args']>
-  ): Promise<void> {
-    logger.info('handleRewardsClaimed');
-    assert(event.args, 'No event args');
-
-    const { orderId, sender, tokenGive, tokenGet, amountGive, amountGet, expireDate } = event.args;
-
-    const order = Order.create({
-        id: orderId.toString(),
-        sender,
-        tokenGive,
-        tokenGet,
-        amountGive: amountGive.toBigInt(),
-        amountGet: amountGet.toBigInt(),
-        expireDate: new Date(expireDate.toNumber() * 1000), // seconds from contract
-        amountGiveLeft: amountGive.toBigInt(),
-        status: ACTIVE,
-        createAt: getUpsertAt('handleExchangeOrderSent', event)
-    });
-
-    await order.save();
-}
-
-export async function handleTrade(
-    event: AcalaEvmEvent<TradeEvent['args']>
-){
-    logger.info('handleTrade');
-    assert(event.args, 'No event args');
-
-    const sender = event.from;
-    const { orderId } = event.args;
-    
-    const handlerInfo = getUpsertAt('handleTrade', event);
-
-    const permissionedExchange = PermissionedExchange__factory.connect(
-        EXCHANGE_DIST_ADDRESS,
-        new FrontierEthProvider()
-    );
-    
-    const { amountGiveLeft } = await permissionedExchange.orders(orderId);    
-    await createTrade(orderId, sender, event);
-
-    //-- Order Entity handling 
-
-    const order = await Order.get(orderId.toString());
-    assert(order, `Expect order with id ${orderId.toString()} to exist`);
-
-    order.amountGiveLeft = amountGiveLeft.toBigInt();
-    order.updateAt = handlerInfo;
-    await order.save();
-
-    //-- Trader Entity handling 
-
+async function createOrUpdateTrader(
+    sender: string,
+    handlerInfo: string,
+    event: FrontierEvmEvent<TradeEvent['args']>
+) {
     let trader = await Trader.get(sender);
     const totalTradeAmount = calculateTradeAmount(trader?.totalTradeAmount ?? BigInt(0), event);
 
@@ -118,19 +72,117 @@ export async function handleTrade(
     await trader.save();
 }
 
+// MAPPING HANDLERS
+
+export async function handleExchangeOrderSent(
+    event: FrontierEvmEvent<ExchangeOrderSentEvent['args']>
+  ): Promise<void> {
+    logger.info('handleExchangeOrderSent');
+    assert(event.args, 'No event args');
+
+    const { orderId, sender, tokenGive, tokenGet, amountGive, amountGet, expireDate } = event.args;
+    const permissionedExchange = PermissionedExchange__factory.connect(
+        EXCHANGE_DIST_ADDRESS,
+        new FrontierEthProvider()
+    );
+
+    const { pairOrderId, tokenGiveBalance } = await permissionedExchange.orders(orderId);
+
+    const order = Order.create({
+        id: orderId.toString(),
+        sender,
+        tokenGive,
+        tokenGet,
+        pairOrderId: pairOrderId.toBigInt(),
+        amountGive: amountGive.toBigInt(),
+        amountGet: amountGet.toBigInt(),
+        expireDate: new Date(expireDate.toNumber() * 1000), // seconds from contract
+        tokenGiveBalance: tokenGiveBalance.toBigInt(),
+        status: ACTIVE,
+        createAt: getUpsertAt('handleExchangeOrderSent', event)
+    });
+
+    await order.save();
+}
+
+async function updateOrder(orderId: BigInt, permissionedExchange: PermissionedExchange, handlerInfo: string) {
+    // orderId start from `1`
+    if (orderId === BigInt(0)) return;
+
+    const { tokenGiveBalance } = await permissionedExchange.orders(orderId.toString());    
+    const order = await Order.get(orderId.toString());
+    assert(order, `Expect order with id ${orderId.toString()} to exist`);
+
+    order.tokenGiveBalance = tokenGiveBalance.toBigInt();
+    order.updateAt = handlerInfo;
+    await order.save();
+}
+
+export async function handleTrade(
+    event: FrontierEvmEvent<TradeEvent['args']>
+): Promise<void> {
+    logger.info('handleTrade');
+    assert(event.args, 'No event args');
+
+    const { orderId } = event.args;
+    const handlerInfo = getUpsertAt('handleTrade', event);
+
+    //-- Trade Entitiy handling
+    const permissionedExchange = PermissionedExchange__factory.connect(
+        EXCHANGE_DIST_ADDRESS,
+        new FrontierEthProvider()
+    );
+
+    //-- Trader and Trade Entitiy handling
+    const { sender, pairOrderId } = await permissionedExchange.orders(orderId);    
+    await createOrUpdateTrader(sender, handlerInfo, event);
+    await createTrade(orderId, sender, event);
+
+    //-- Order Entity handling 
+    await updateOrder(orderId.toBigInt(), permissionedExchange, handlerInfo);
+    await updateOrder(pairOrderId.toBigInt(), permissionedExchange, handlerInfo);
+}
+
 export async function handleOrderSettled(
-    event: AcalaEvmEvent<OrderSettledEvent['args']>
-){
+    event: FrontierEvmEvent<OrderSettledEvent['args']>
+): Promise<void> {
     logger.info('handleOrderSettled');
     assert(event.args, 'No event args');
 
-    const { orderId, amountGive, amountGet } = event.args;
+    const { orderId } = event.args;
     const order = await Order.get(orderId.toString());
     assert(order, `No order found for ${orderId}`)
 
     order.status = INACTIVE;
-    order.amountGive = amountGive.toBigInt();
-    order.amountGet = amountGet.toBigInt();
     order.updateAt = getUpsertAt('handleOrderSettled', event);
     await order.save();
+}
+
+export async function handleQuotaAdded( 
+    event: FrontierEvmEvent<QuotaAddedEvent['args']>
+): Promise<void> {
+    logger.info('handleQuotaAdded');
+    assert(event.args, 'No event args');
+    const { account, amount } = event.args;
+
+    const amountBigInt = amount.toBigInt();
+    const handlerInfo = getUpsertAt('handleQuotaAdded', event);
+
+    let trader = await Trader.get(account);
+
+    if (!trader) {
+        trader = Trader.create({
+            id: account,
+            totalTradeAmount: BigInt(0),
+            totalAwardAmount: amountBigInt,
+            maxTradeAmount: amountBigInt,
+            createAt: handlerInfo 
+        });
+    } else {
+        const totalAwardAmount = trader.totalAwardAmount + amountBigInt
+        trader.totalAwardAmount = totalAwardAmount; 
+        trader.maxTradeAmount = totalAwardAmount - trader.totalTradeAmount;   
+    }
+    
+    await trader.save();
 }
--- src/mappings/utils/index.ts
+++ src/mappings/utils/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { AcalaEvmEvent } from '@subql/acala-evm-processor';
-import testnetAddresses from '@subql/contract-sdk/publish/testnet.json';
+import { FrontierEvmEvent } from '@subql/frontier-evm-processor';
+import testnetAddresses from '@subql/contract-sdk/publish/moonbase.json';
 
 export const EXCHANGE_DIST_ADDRESS = testnetAddresses.PermissionedExchange.address;
 export const KSQT_ADDRESS = testnetAddresses.SQToken.address;
@@ -13,9 +13,8 @@ export const KSQT_ADDRESS = testnetAddresses.SQToken.address;
  * @param event
  * @returns `${topicHandler:block:Txhash}`
  */
-export const getUpsertAt = (handler: string, event: AcalaEvmEvent<any>): string => {
-  const upsertAt = `${handler}:${event.blockNumber}:${event.transactionHash}`;
-  return upsertAt;
+export const getUpsertAt = (handler: string, event: FrontierEvmEvent<any>): string => {
+  return `${handler}:${event.blockNumber}:${event.transactionHash}`;
 };
 
 export const isKSQT = (address: string): boolean => {
